2403c7cbc8302f395caeccf2d057648158ce8fc2
Daniel Jamsheedy <djamsheedy99@gmail.com>
2020-11-09 16:52:02 -0500

    Base Project. Partially completed login screen.

---
+++ Pods/Target Support Files/Pods-RunnR-RunnRUITests/Pods-RunnR-RunnRUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RunnR_RunnRUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RunnR_RunnRUITests
+@end
---
+++ RunnR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RunnR
+//
+//  Created by MaKayla Day on 11/5/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
